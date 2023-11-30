177dca02ca75cfa1669fe55e847737af8876a4f2
Obaedur Rahman <arnobahemd12345@gmail.com>
2023-11-30 02:13:14 -0500

    Deleted some files

--- src/CodeforcesRakib/Watermelon4A.java
+++ src/CodeforcesRakib/Watermelon4A.java
@@ -1,20 +1,21 @@
 package CodeforcesRakib;
+
 import java.util.Scanner;
+
 public class Watermelon4A {
 
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-            // Read the weight of the watermelon
-            int w = scanner.nextInt();
+        // Read the weight of the watermelon
+        int w = scanner.nextInt();
 
-            // Check if it's possible to divide the watermelon into two even parts
-            if (w > 2 && w % 2 == 0) {
-                System.out.println("YES");
-            } else {
-                System.out.println("NO");
-            }
-
-            scanner.close();
+        // Check if it's possible to divide the watermelon into two even parts
+        if (w > 2 && w % 2 == 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
         }
+        scanner.close();
     }
+}
