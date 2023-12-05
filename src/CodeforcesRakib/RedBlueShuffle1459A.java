b43689e82f9a0f4b5aca14586fb0e1116018313a
rakib <rakibuddin.md@gmail.com>
2023-12-05 05:57:47 -0500

    red blue

---
+++ src/CodeforcesRakib/RedBlueShuffle1459A.java
@@ -0,0 +1,37 @@
+package CodeforcesRakib;
+
+
+import java.util.Scanner;
+
+public class RedBlueShuffle1459A {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            int rc = 0, bc = 0;
+            int n = scanner.nextInt();
+            String r = scanner.next();
+            String b = scanner.next();
+
+            for (int i = 0; i < n; i++) {
+                if (r.charAt(i) > b.charAt(i))
+                    rc++;
+                else if (r.charAt(i) < b.charAt(i))
+                    bc++;
+                else {
+                    rc++;
+                    bc++;
+                }
+            }
+
+            if (rc > bc)
+                System.out.println("RED");
+            else if (bc > rc)
+                System.out.println("BLUE");
+            else
+                System.out.println("EQUAL");
+        }
+    }
+}
